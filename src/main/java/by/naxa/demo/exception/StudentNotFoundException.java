2545f52b6484e155ad2e5f34eafd807b1089fa72
naXa777 <alarmo.lord@gmail.com>
2015-03-17 18:12:09 +0300

    Documented StudentNotFoundException.

--- src/main/java/by/naxa/demo/exception/StudentNotFoundException.java
+++ src/main/java/by/naxa/demo/exception/StudentNotFoundException.java
@@ -3,6 +3,8 @@ package by.naxa.demo.exception;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 /**
+ * Thrown to indicate that a method has failed to find a student.
+ *
  * Created on 10.03.2015.
  * @author phomal
  */
@@ -10,6 +12,11 @@ public class StudentNotFoundException extends EmptyResultDataAccessException {
 
 	private static final long serialVersionUID = -81460315521662721L;
 
+	/**
+	 * Constructs a new exception with a detail message, which contains the specified id.
+	 *
+	 * @param  id the ID of a non-existent Student entity.
+	 */
 	public StudentNotFoundException(Long id) {
 		super(String.format("No Student entity with id %s exists!", id), 1);
 	}
